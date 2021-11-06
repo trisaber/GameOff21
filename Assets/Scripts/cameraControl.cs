01d1ab436ee935d92ea2351d022468a5eee34245
ilhamucok <44099610+ilhamucok@users.noreply.github.com>
2021-11-06 19:40:14 +0300

    GO21-2
    mevcut kod kopyalandı.

---
+++ Assets/Scripts/cameraControl.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraControl : MonoBehaviour
+{
+
+    [SerializeField] Transform player;
+    [SerializeField] float smoothSpeed=.6f;
+    [SerializeField] Vector3 offset;
+    
+
+
+
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 camPosition = player.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, camPosition, smoothSpeed);
+        transform.position = smoothedPosition;
+        transform.LookAt(player);
+
+    
+    }
+}
