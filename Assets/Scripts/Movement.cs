3f13b21be3ba4944a27cb8e9cb1f7d77b8e42741
Tolga Cevik <81570992+tolgacevik@users.noreply.github.com>
2021-11-10 03:35:46 +0300

    GO21-5 first commit about movements A movement state machine is implemented

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -16,34 +16,42 @@ public class Movement : MonoBehaviour
     [SerializeField] LayerMask ledgeLayer;
     [SerializeField] Transform ledgeChecker;
 
-    bool isHanging = false;
-    bool isGrounded;
+    [SerializeField] bool isHanging = false;
+    [SerializeField] bool isGrounded;
     public Animator animator;
     public Transform model;
     public float deltaX;
 
-
-
+    PlayerAbstractState state;
 
 
     #endregion
 
     void Start()
     {
-
+        TransitionTo(new PlayerMovementState());
     }
-    void Update()
+
+    void FixedUpdate()
+    {
+        state.Move();
+        state.Jump();
+
+        controller.Move(direction);
+    }
+
+    void Update2()
     {
         isGrounded = Physics.CheckSphere(groundCheck.position, 0.15f, groundLayer);
-        if (isGrounded&!isHanging)  //checks if the player is grounded
+        if (isGrounded & !isHanging)  //checks if the player is grounded
         {
             animator.SetBool("isJump", false);
-             animator.SetBool("isGrounded", true);
-             direction.y = 0;
-             Move();
-             Jump();
+            animator.SetBool("isGrounded", true);
+            direction.y = 0;
+            Move();
+            Jump();
         }
-        else if(isHanging)
+        else if (isHanging)
         {
             direction.y = 0;
             direction.x = 0;
@@ -55,11 +63,10 @@ public class Movement : MonoBehaviour
             direction.y += gravity * Time.deltaTime;
         }
         controller.Move(direction);
-
     }
     private void Climb()
     {
-        if(Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump"))
         {
             isHanging = false;
             Jump();
@@ -73,37 +80,43 @@ public class Movement : MonoBehaviour
             direction.y += gravity * Time.deltaTime;
         }
     }
+
     private void Move()
     {
-        deltaX = Input.GetAxis("Horizontal") ;
+        deltaX = Input.GetAxis("Horizontal");
         direction.x = deltaX * Movespeed * Time.deltaTime;
 
         animator.SetFloat("speed", Mathf.Abs(deltaX));// spped run animationı çalıştırıyor.
-        if (deltaX!=0)
+        if (deltaX != 0)
         {
             Quaternion newRotation = Quaternion.LookRotation(new Vector3(deltaX, 0, 0));
             model.rotation = newRotation;
         }
-
     }
+
     private void Jump()
     {
-            if (Input.GetButtonDown("Jump"))
-            {
-
-                animator.SetBool("isGrounded", false);
-                //direction.y = JumpForce * Time.deltaTime;
-                isHanging = Physics.CheckSphere(ledgeChecker.position, 0.15f, ledgeLayer);
-                animator.SetBool("isHanging", isHanging);
-                animator.SetBool("isJump", true);
+        if (Input.GetButtonDown("Jump"))
+        {
+            animator.SetBool("isGrounded", false);
+            //direction.y = JumpForce * Time.deltaTime;
+            isHanging = Physics.CheckSphere(ledgeChecker.position, 0.15f, ledgeLayer);
+            animator.SetBool("isHanging", isHanging);
+            animator.SetBool("isJump", true);
         }
-        
+
     }
     public void Hanged()
     {
         isHanging = true;
         animator.SetBool("isHanging", isHanging);
     }
+
+    private void Climbing()
+    {
+        transform.position = new Vector3(transform.position.x, transform.position.y + 3, transform.position.z);
+    }
+
     public void Climbed()
     {
         transform.position = new Vector3(transform.position.x, transform.position.y + 3, transform.position.z);
@@ -111,5 +124,169 @@ public class Movement : MonoBehaviour
         animator.SetBool("isHanging", isHanging);
         animator.SetBool("isJump", false);
     }
+
+    private void TransitionTo(PlayerAbstractState newState)
+    {
+        state = newState;
+        state.SetContext(this);
+        state.SetAnimatorStates();
+    }
+
+
+    private abstract class PlayerAbstractState
+    {
+        protected Movement context;
+
+        public void SetContext(Movement _context)
+        {
+            this.context = _context;
+        }
+
+        // Start is called before the first frame update
+        public abstract void Move();
+        public abstract void Jump();
+        public abstract void SetAnimatorStates();
+
+        protected void SetAnimatorStates(bool isGrounded, bool isHanging, bool isJump)
+        {
+            context.animator.SetBool("isGrounded", isGrounded);
+            context.animator.SetBool("isHanging", isHanging);
+            context.animator.SetBool("isJump", isJump);
+
+        }
+    }
+
+    private class PlayerMovementState : PlayerAbstractState
+    {
+        public override void Jump()
+        {
+            if (Input.GetButtonDown("Jump"))
+            {
+                if (Physics.CheckSphere(context.ledgeChecker.position, 0.15f, context.ledgeLayer))
+                {
+                    context.TransitionTo(new PlayerMovementJumpState());
+                }
+                else
+                {
+                    context.TransitionTo(new PlayerMovementJumpAndFallState());
+                }
+            }
+        }
+
+        public override void Move()
+        {
+            JumpAndFallAnimation jafa = context.animator.GetBehaviour<JumpAndFallAnimation>();
+            ClimbAnimation ca = context.animator.GetBehaviour<ClimbAnimation>();
+            if (jafa.inAnimation == false && ca.inAnimation == false)
+            {
+                context.Move();
+            }
+        }
+
+        public override void SetAnimatorStates()
+        {
+            Debug.Log("PlayerMovementState::SetAnimatorStates");
+
+            SetAnimatorStates(true, false, false);
+        }
+    }
+
+
+    private class PlayerMovementJumpState : PlayerAbstractState
+    {
+        public override void Jump()
+        {
+            if (Input.GetAxis("Vertical") > 0)
+            {
+                context.TransitionTo(new PlayerMovementClimbState());
+            }
+            else if (Input.GetAxis("Vertical") < 0)
+            {
+                context.TransitionTo(new PlayerMovementFallState());
+            }
+        }
+
+        public override void Move()
+        {
+            context.direction.Set(0, 0, 0);
+        }
+
+        public override void SetAnimatorStates()
+        {
+            Debug.Log("PlayerMovementJumpState::SetAnimatorStates");
+
+            SetAnimatorStates(false, true, true);
+        }
+    }
+
+    private class PlayerMovementJumpAndFallState : PlayerAbstractState
+    {
+        public override void Jump()
+        {
+        }
+
+        public override void Move()
+        {
+            context.direction.Set(0, 0, 0);
+            if (context.animator.GetBehaviour<JumpAndFallAnimation>().inAnimation)
+            {
+                context.TransitionTo(new PlayerMovementState());
+            }
+        }
+
+        public override void SetAnimatorStates()
+        {
+            Debug.Log("PlayerMovementJumpAndFallState::SetAnimatorStates");
+
+            SetAnimatorStates(false, false, true);
+        }
+    }
+
+    private class PlayerMovementClimbState : PlayerAbstractState
+    {
+        public override void Jump()
+        {
+        }
+
+        public override void Move()
+        {
+            context.direction.Set(0, 0, 0);
+            if( context.animator.GetBehaviour<ClimbAnimation>().inAnimation) {
+                context.Climbing();
+                context.TransitionTo(new PlayerMovementState());
+            }
+        }
+
+        public override void SetAnimatorStates()
+        {
+            Debug.Log("PlayerMovementClimbState::SetAnimatorStates");
+
+            SetAnimatorStates(false, true, false);
+        }
+    }
+
+    private class PlayerMovementFallState : PlayerAbstractState
+    {
+        public override void Jump()
+        {
+        }
+
+        public override void Move()
+        {
+            context.direction.Set(0, 0, 0);
+            if (context.animator.GetBehaviour<ClimbAnimation>().inAnimation)
+            {
+                context.TransitionTo(new PlayerMovementState());
+            }
+        }
+
+        public override void SetAnimatorStates()
+        {
+            Debug.Log("PlayerMovementClimbState::SetAnimatorStates");
+
+            SetAnimatorStates(false, true, false);
+        }
+    }
+
 }
 
