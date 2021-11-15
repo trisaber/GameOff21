7379cfadc0951920d64d40f6c906632739269122
Tolga Cevik <81570992+tolgacevik@users.noreply.github.com>
2021-11-15 03:29:34 +0300

    GO21-6 Protagonist is added to use newAnimations Committed animations are copied from the main fbx in newAnimations to enable editting animations settings new animator is added there are some problems with crouch related animations.

---
+++ Assets/Scripts/ProtagonistAnimationSync.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animationSync : MonoBehaviour
+{
+    public Movement player;
+    void Start()
+    {
+        
+    }
+    public void ClimbedUp()
+    {
+        // player.Climbing();
+        player.state.EndOfAnimation();
+    }
+
+    public void JumpMissedGrab()
+    {
+        player.state.EndOfAnimation();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
