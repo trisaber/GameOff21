9537968da0c2ea9d0790da453399d0d237c94d15
ilhamucok <44099610+ilhamucok@users.noreply.github.com>
2021-11-16 02:25:15 +0300

    Working basic inventory system
    ...Picking up collectables
    ...Cheking multiple pickups and full inventory
    ...Activates Personal assistance panel when certain type collectable is  picked.

---
+++ Assets/Scripts/Collectable.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectable : MonoBehaviour
+{
+    public enum collectable
+    {
+        CollectabeA,
+        CollectabeB,
+        CollectabeC,
+        CollectabeD
+    };
+    public collectable collectableType = collectable.CollectabeA;
+    public bool picked=false;
+
+
+}
