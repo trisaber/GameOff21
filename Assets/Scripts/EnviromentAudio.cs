8a978538d9f8765fab4523373243ba9d964c73fb
ilhamucok <44099610+ilhamucok@users.noreply.github.com>
2021-11-20 22:00:27 +0300

    Terrain Source and item source ready
    working basic audio system

---
+++ Assets/Scripts/EnviromentAudio.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnviromentAudio : MonoBehaviour
+{
+    public AudioClip[] clips;
+    AudioSource audioSource;
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.loop = false;
+        randomPlay();
+    }
+
+    void  randomPlay()
+    {
+
+            StartCoroutine(playAudio());
+    }
+    IEnumerator playAudio()
+
+    {
+        audioSource.clip = clips[Random.Range(0, clips.Length)];
+        audioSource.Play();
+        yield return new WaitForSeconds(audioSource.clip.length);
+        randomPlay();
+    }
+}
