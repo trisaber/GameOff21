1ebf3d4defcca32e4ccc138118a5018855ad2fec
ilhamucok <44099610+ilhamucok@users.noreply.github.com>
2021-11-22 13:43:17 +0300

    VirtualCams and Sounds working proper
    mainCam audio listerner delete.
    add player object audio listerner.
    camera positionas and blends can change.

--- Assets/Scripts/cameraControl.cs
+++ Assets/Scripts/cameraControl.cs
@@ -1,26 +1,57 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class cameraControl : MonoBehaviour
 {
-
+    /*
     [SerializeField] Transform player;
     [SerializeField] float smoothSpeed=.6f;
     [SerializeField] Vector3 offset;
-    
+    */
+
+    [SerializeField]
+    private CinemachineVirtualCamera gameCam;
+
+    [SerializeField]
+    private CinemachineVirtualCamera zoomCam;
 
 
 
 
-    // Update is called once per frame
-    void FixedUpdate()
+    /*
+     void FixedUpdate()
+     {
+         Vector3 camPosition = player.position + offset;
+         Vector3 smoothedPosition = Vector3.Lerp(transform.position, camPosition, smoothSpeed);
+         transform.position = smoothedPosition;
+         transform.LookAt(player);
+
+
+     }
+     */
+    private void FixedUpdate()
     {
-        Vector3 camPosition = player.position + offset;
-        Vector3 smoothedPosition = Vector3.Lerp(transform.position, camPosition, smoothSpeed);
-        transform.position = smoothedPosition;
-        transform.LookAt(player);
+        if (Input.GetButtonDown("Fire1"))
+        {
+            ZoomCam();
+        }
+        if (Input.GetButtonUp("Fire1"))
+        {
+            GameCam();
+        }
+    }
 
-    
+    private void GameCam()
+    {
+        zoomCam.Priority = 5;
+        gameCam.Priority = 60;
+    }
+
+    private void ZoomCam()
+    {
+        gameCam.Priority = 5;
+        zoomCam.Priority = 60;
     }
 }
