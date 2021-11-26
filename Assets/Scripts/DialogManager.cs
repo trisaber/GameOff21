5dd9d5e1c01de33d67868398d75723af2b73301c
ilhamucok <44099610+ilhamucok@users.noreply.github.com>
2021-11-26 15:01:23 +0300

    test for dialog box
    dialogBox take text from language scriptable object

---
+++ Assets/Scripts/DialogManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogManager : MonoBehaviour
+{
+    public Text text;
+    public float waitTime=10f;
+
+    public NewLanguage language;
+
+    private void Start()
+    {        
+        new WaitUntil(() => Input.anyKey);
+        StartCoroutine(changeText(language.firstCall)) ;
+        //StartCoroutine(changeText(language.lavaReCall));
+    }
+
+    IEnumerator changeText(string inputText)
+    {
+        text.text = inputText;
+        
+        yield return new WaitUntil(()=>Input.anyKey);
+        text.text = language.lavaReCall;
+    }
+
+}
---
+++ Assets/Scripts/NewLanguage.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName ="New Language")]
+public class NewLanguage : ScriptableObject
+{
+    [TextArea(5, 10)]
+    public string firstCall;
+
+    [TextArea(5, 10)]
+    public string standUpCall;
+
+    [TextArea(5, 10)]
+    public string lavaReCall;
+
+    [TextArea(5, 10)]
+    public string arne;
+
+    [TextArea(5, 10)]
+    public string noteBook;
+}
