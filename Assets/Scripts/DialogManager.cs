33273449307f72a473974d3b36fb4c540d15c3be
ilhamucok <44099610+ilhamucok@users.noreply.github.com>
2021-11-26 20:17:06 +0300

    firstcall and lavaRecall
    lava recall and first key press call are ready

--- Assets/Scripts/DialogManager.cs
+++ Assets/Scripts/DialogManager.cs
@@ -7,22 +7,52 @@ public class DialogManager : MonoBehaviour
 {
     public Text text;
     public float waitTime=10f;
+    public Transform lava;
+    bool lavaFound = false;
 
     public NewLanguage language;
 
     private void Start()
-    {        
+    {
+        StartCoroutine(startLava());
+    }
+
+    IEnumerator startLava()
+    {
         new WaitUntil(() => Input.anyKey);
-        StartCoroutine(changeText(language.firstCall)) ;
-        //StartCoroutine(changeText(language.lavaReCall));
+        yield return StartCoroutine(changeText(language.firstCall));
+        yield return new WaitForSeconds(waitTime);
+        yield return StartCoroutine(changeText(language.standUpCall));
     }
 
     IEnumerator changeText(string inputText)
     {
-        text.text = inputText;
+        
         
         yield return new WaitUntil(()=>Input.anyKey);
-        text.text = language.lavaReCall;
+        text.text = inputText;
+        StartCoroutine(clearText());
     }
 
+    IEnumerator clearText()
+    {
+        yield return new WaitForSeconds(waitTime);
+        text.text = "  ";
+    }
+    private void Update()
+    {
+        lavaCall();
+    }
+
+    private void lavaCall()
+    {
+        if (Vector3.Distance(transform.position, lava.position) < 5)
+        {
+            lavaFound = true;
+        }
+        if (lavaFound && Vector3.Distance(transform.position, lava.position) > 5)
+        {
+            StartCoroutine(changeText(language.lavaReCall));
+        }
+    }
 }
