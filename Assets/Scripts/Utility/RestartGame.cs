988fb943b71111d93cd04c35883130be694bdace
Tolga Cevik <81570992+tolgacevik@users.noreply.github.com>
2021-12-01 17:29:07 +0300

    GO21-42 infratstucture to restart game with double ESC tap

---
+++ Assets/Scripts/Utility/RestartGame.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RestartGame : MonoBehaviour
+{
+    public float tapSpeed = 0.25f; //in seconds
+    private float lastTapTime = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lastTapTime = 0;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+
+            if ((Time.time - lastTapTime) <= tapSpeed)
+            {
+                // TODO tolga restart the game
+            }
+
+            lastTapTime = Time.time;
+        }
+    }
+}
